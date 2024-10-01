b6eaa0236908b9dd8f14e672e7f5a4a1c00fc61d
Julien Loridant <jloridant@iutbayonne.univ-pau.fr>
2024-10-01 11:18:20 +0200

    Mise en place du test avec une valeur (2) et validation du test

--- src/main/java/FacteursPremiers/FacteursPremiers.java
+++ src/main/java/FacteursPremiers/FacteursPremiers.java
@@ -6,12 +6,33 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FacteursPremiers {
+    static Set<Integer> generate(int nb) {
+        if (nb > 0) {
+            HashSet<Integer> listFP = new HashSet<Integer>();
 
-    public static Set<Integer> generate(int nb){
+            // Diviser le nombre par 2 tant qu'il est pair
+            while (nb % 2 == 0) {
+                listFP.add(2);
+                nb /= 2;
+            }
 
-        Set<Integer> listeNB = new HashSet<Integer>();
-        return listeNB;
+            // Diviser le nombre par les nombres impairs à partir de 3
+            for (int i = 3; i <= Math.sqrt(nb); i += 2) {
+                while (nb % i == 0) {
+                    listFP.add(i);
+                    nb /= i;
+                }
+            }
 
+            // Si le nombre est un facteur premier supérieur à 2
+            if (nb > 2) {
+                listFP.add(nb);
+            }
+
+            return listFP;
+        }
+        else {
+            throw new RuntimeException("Le nombre doit être supérieur à 0");
+        }
     }
-
 }
